9e1c134fe39e93d4f35b42db4c54b3c71b501294
ilhamcendana <ilhamcendana98@gmail.com>
2022-06-17 23:53:11 +0700

    feat: set react-toastify

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Layout from 'Components/Layout';
 import './App.css';
 import Routers from './Routers';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
-      <Layout>
-        <Routers />
-      </Layout>
+      <Routers />
+      <ToastContainer />
     </div>
   );
 }
