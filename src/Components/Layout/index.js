390e7765c28b91db1d3752d4758b625dc744784f
ilhamcendana <ilhamcendana98@gmail.com>
2022-06-17 23:53:35 +0700

    feat: set layout as wrapper

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,8 +1,19 @@
+import { useLayoutEffect } from "react";
+import { useEffect } from "react";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 
-const Layout = ({ children }) => {
+const Layout = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+
+    useEffect(() => {
+        window.scrollTo({ top: 0 ,behavior:'smooth'})
+    }, [location])
     return (
-        <div className="">
-            {children}
+        <div>
+            {location.pathname !== '/' && <button onClick={() => navigate(-1)} className="btn btn-outline normal-case fixed top-8 left-8 z-30">Back</button>}
+            <Outlet />
         </div>
     );
 }
--- src/Routers.js
+++ src/Routers.js
@@ -1,14 +1,23 @@
+import Layout from "Components/Layout";
 import {
     Routes,
     Route,
+    Navigate,
 } from "react-router-dom";
+import Detail from "Views/Detail";
 import Home from "Views/Home";
+import NotFound from "Views/NotFound";
 
 
 const Routers = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route element={<Layout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/detail/:id/:tab" element={<Detail />} />
+            </Route>
+            <Route path='/404' element={<NotFound />} />
+            <Route path="*" element={<Navigate to={'/404'} />} />
         </Routes>
     )
 }
